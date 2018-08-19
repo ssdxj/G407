405e37944d3caaf379a9477113435b97dce300bd
LiLab <purewangli@gmail.com>
2018-08-19 10:44:45 +0800

    update modelInfo_bear

--- R/modelInfo_bear.R
+++ R/modelInfo_bear.R
@@ -15,25 +15,21 @@ modelInfo_bear$grid <- function(x, y, len = NULL, search = "grid") data.frame(pa
 
 # fit
 modelInfo_bear$fit <- function(x, y, wts, param, lev, last, classProbs, ...) {
-  param <- list(...)
-  x_max <- param$x_max
-  x_min <- param$x_min
+ dat <- data.frame(x = x)
+ dat$.outcome <- y
 
-  dat <- data.frame(x = x)
-  dat$.outcome <- y
-  x_upper <- max(x)
-  x_lower <- min(x)
-  x_sd <- sd(x)
-  out <- nlsLM(y ~ -1 / (K_VI) * log((VI_inf - x) / (VI_inf - VI_bare)),
-    data = data.frame(x = x, y = y),
-    trace = FALSE,
-    control = nls.lm.control(maxiter = 1024),
-    start = c(VI_inf = x_upper + x_sd * 3, VI_bare = x_lower - x_sd * 3, K_VI = 0.2),
-    lower = c(VI_inf = x_max, VI_bare = x_lower - x_sd * 10, K_VI = -1),
-    upper = c(VI_inf = x_upper + x_sd * 10, VI_bare = x_min, K_VI = 3)
+ x_upper <- max(x)
+ x_lower <- min(x)
+ x_sd <- sd(x)
+
+ nlsLM(.outcome ~ -1 / (K_VI) * log((VI_inf - x) / (VI_inf - VI_bare)),
+              data = dat,
+              trace = FALSE,
+              control = nls.lm.control(maxiter = 1024),
+              start = c(VI_inf = x_upper + x_sd * 3, VI_bare = x_lower - x_sd * 3, K_VI = 0.2),
+              lower = c(VI_inf = x_max, VI_bare = x_lower - x_sd * 10, K_VI = -1),
+              upper = c(VI_inf = x_upper + x_sd * 10, VI_bare = x_min, K_VI = 3)
   )
-
-  return(out)
 }
 
 # predict
