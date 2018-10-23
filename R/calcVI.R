98b3ca8186a99aa14d475e18bc38bec1f7ee8aed
ssdxj <purewangli@gmail.com>
2018-10-23 20:56:16 +0800

    update calcVI

--- R/calcVI.R
+++ R/calcVI.R
@@ -11,12 +11,9 @@ calcVI <- function(index, spc, ...) {
   if(index %in% vegindex()){
     out <- vegindex(spc, index, ...)
   } else {
-    wl_NIR <- ifelse(is.null(param$NIR), 800, as.double(param$NIR))
-    wl_red <- ifelse(is.null(param$red), 670, as.double(param$red))
-    wl_green <- ifelse(is.null(param$green), 550, as.double(param$green))
-    bNIR <- get_reflectance(spc, wl_NIR, weighted = weighted)
-    bR <- get_reflectance(spc, wl_red, weighted = weighted)
-    bG <- get_reflectance(spc, wl_green, weighted = weighted)
+    bNIR <- get_reflectance(spc, wavelength = 830, weighted = weighted)
+    bR <- get_reflectance(spc, wavelength = 670, weighted = weighted)
+    bG <- get_reflectance(spc, wavelength = 550, weighted = weighted)
 
     if(index == 'Green_NDVI'){
       out <- vegindex(spc, 'Green NDVI', weighted = weighted)
@@ -40,7 +37,10 @@ calcVI <- function(index, spc, ...) {
     } else if(index == 'CI_Green'){
       # Gitelson et al, (2003)
       out <- bNIR / bG - 1
-    } else if(index == 'MCARI_Hab'){
+    } else if(index == 'CI_re'){
+      re <- get_reflectance(spc, wavelength = 743, weighted = weighted)
+      out <- bNIR / re - 1
+    } else if(index == 'MCARI_hab'){
       # Haboudane et al.(2004)
       num <- 1.5 * (2.5 * (bNIR - bR) - 1.3 * (bNIR - bG))
       deo <- sqrt(2 * (bNIR + 1)^2 + 6 * bNIR + 5 * sqrt(bR) - 0.5)
@@ -49,9 +49,11 @@ calcVI <- function(index, spc, ...) {
       num <- 1.5 * (1.2 * (bNIR - bG) - 2.5 * (bR - bG))
       deo <- sqrt(2 * (bNIR + 1)^2 + 6 * bNIR + 5 * sqrt(bR) - 0.5)
       out <- num / deo
-    } else if(index == 'MCARI2_wu'){
-      r750 <- get_reflectance(spc, 750, weighted = weighted)
-      r705 <- get_reflectance(spc, 750, weighted = weighted)
+    } else if(index == 'MCARI_wu2008'){
+      out <- vegindex(spc, 'MCARI2', weighted = weighted)
+    } else if(index == 'MCARI_wu2010'){
+      r750 <- get_reflectance(spc, wavelength =  750, weighted = weighted)
+      r705 <- get_reflectance(spc, wavelength = 705, weighted = weighted)
       num <- 1.5 * (2.5 * (r750 - r705) - 1.3 * (r750 - bG))
       deo <- sqrt(2 * (r750 + 1)^2 + 6 * r750 + 5 * sqrt(r705) - 0.5)
       out <- num / deo
@@ -67,20 +69,35 @@ calcVI <- function(index, spc, ...) {
       out <- rededge(spc)$Rp
     } else if (index == "Rs") {
       out <- rededge(spc)$Rs
-    }  else if (str_detect(index, "^opt_NDVI")) { # incase NRI calc by hsdar::nri
+    }  else if (str_detect(index, "^NDVI_\\d+")) { # incase NRI calc by hsdar::nri
       # wls <- str_extract_all(index, '\\d+(.\\d+)?') %>% unlist() %>% as.numeric()
-      wls <- str_split(index, "_")[[1]][c(3, 4)] %>% parse_double()
-      if (length(wls) != 2) stop("wrong NDSI index name!!!")
-      b1 <- get_reflectance(spc, wls[1], weighted = FALSE)
-      b2 <- get_reflectance(spc, wls[2], weighted = FALSE)
+      wls <- str_split(index, "_")[[1]][c(2, 3)] %>% parse_double()
+      if (length(wls) != 2) {
+        msg <- glue::glue('error: {index} parsed wl is not with length 2({wls})')
+        stop(msg)
+      }
+      b1 <- get_reflectance(spc, wavelength = wls[1], weighted = FALSE)
+      b2 <- get_reflectance(spc, wavelength = wls[2], weighted = FALSE)
       out <- (b2 - b1) / (b2 + b1)
-    } else if (str_detect(index, "^opt_DVI")) { # incase NRI calc by hsdar::nri
+    } else if (str_detect(index, "^DVI_\\d+")) { # incase NRI calc by hsdar::nri
       # wls <- str_extract_all(index, '\\d+(.\\d+)?') %>% unlist() %>% as.numeric()
-      wls <- str_split(index, "_")[[1]][c(3, 4)] %>% parse_double()
-      if (length(wls) != 2) stop("wrong NDSI index name!!!")
-      b1 <- get_reflectance(spc, wls[1], weighted = FALSE)
-      b2 <- get_reflectance(spc, wls[2], weighted = FALSE)
+      wls <- str_split(index, "_")[[1]][c(2, 3)] %>% parse_double()
+      if (length(wls) != 2){
+        msg <- glue::glue('error: {index} parsed wl is not with length 2({wls})')
+        stop(msg)
+      }
+      b1 <- get_reflectance(spc, wavelength = wls[1], weighted = FALSE)
+      b2 <- get_reflectance(spc, wavelength = wls[2], weighted = FALSE)
       out <- b2 - b1
+    } else if(str_detect(index, "^SR_\\d+")){
+       wls <- str_split(index, "_")[[1]][c(2, 3)] %>% parse_double()
+      if (length(wls) != 2){
+        msg <- glue::glue('error: {index} parsed wl is not with length 2({wls})')
+        stop(msg)
+      }
+      b1 <- get_reflectance(spc, wavelength = wls[1], weighted = FALSE)
+      b2 <- get_reflectance(spc, wavelength = wls[2], weighted = FALSE)
+      out <- b1/b2
     } else {
       msg <- glue::glue('Not defined index: {index}!!!')
       stop(msg)
